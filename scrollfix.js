d6200f040b30f6aab6fe9cec4773ab36613af595
Alberto Gasparin <albertogasparin@gmail.com>
2012-06-29 14:52:14 +0200

    Prevent page scroll where the content is smaller than visible area

--- scrollfix.js
+++ scrollfix.js
@@ -9,7 +9,7 @@
 
 var ScrollFix = function(elem) {
 	// Variables to track inputs
-	var startY, startTopScroll = 0;
+	var startY, startTopScroll = 0, disableScroll = false;
 	
 	elem = elem || document.querySelector(elem);
 	
@@ -27,5 +27,15 @@ var ScrollFix = function(elem) {
 
 		if(startTopScroll + elem.clientHeight >= elem.scrollHeight)
 			elem.scrollTop = elem.scrollHeight - elem.clientHeight - 1;
+    
+    disableScroll = (elem.scrollHeight <= elem.clientHeight);
 	}, false);
+  
+  // Page should not scroll when the content does not fill the area
+  elem.addEventListener('touchstart', function(event){
+    if(disableScroll) {
+      event.preventDefault();
+    }
+  }, false);
+  
 };
